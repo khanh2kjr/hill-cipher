bf1619a9efe4d8f61b71add92eeea8812a2957e7
khanhjunior <khanhthachxa@gmail.com>
2021-04-18 12:55:51 +0700

    remove empty string

--- hill.py
+++ hill.py
@@ -5,12 +5,23 @@ import itertools
 
 alphabets = [alphabet for alphabet in ascii_uppercase] #['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
+# Hàm chuyển chuỗi thành mảng ký tự
 def split(word):
     return [char for char in word]
+
+def removeEmptyString(strsArr):
+    result = []
+
+    for i in range(0, len(strsArr)):
+        if (strsArr[i] != ' '):
+            result.append(strsArr[i])
+    
+    return result
 
 def convertToNumbers(char):
     return alphabets.index(char)
 
+# Hàm chia mảng thành các đoạn nhỏ hơn
 def divideTheArray(arr):
     result = []
 
@@ -20,6 +31,7 @@ def divideTheArray(arr):
 
     return result
 
+# Hàm này đóng vai trò mã hóa chính
 def numEncryption(arr, key):
     result = []
 
@@ -40,23 +52,32 @@ def convertToString(alphabetString, plaintextNum):
     return result
 
 def mainEncryption():
+    # Lấy ra giá trị ở 2 ô input bản rõ và khóa K
     inputPlaintext = inpPlaintext.get()
     key = inputKey.get()
-    inpPlaintextArr = split(inputPlaintext) #['N', 'G', 'U', 'Y', 'E', 'N', 'V', 'A', 'N', 'K', 'H', 'A', 'N', 'H']
-    keyArr = split(key) #['A', 'B', 'C', 'D']
+
+    # Chuyển bản rõ và khóa K thành một mảng các ký tự
+    inpPlaintextArr = removeEmptyString(split(inputPlaintext.upper())) #['N', 'G', 'U', 'Y', 'E', 'N', 'V', 'A', 'N', 'K', 'H', 'A', 'N', 'H']
+    keyArr = removeEmptyString(split(key.upper())) #['A', 'B', 'C', 'D']
     
+    # Dựa vào mảng bảng chữ cái alphabets ở đầu chương trình ta chuyển đổi 2 mảng ký tự ở trên thành mảng các số tương ứng từ 0 đến 25 ( dựa vào vị trí của các ký tự trong mảng alphabets )
     inputPlaintextArrNum = list(map(convertToNumbers, inpPlaintextArr)) #[13, 6, 20, 24, 4, 13, 21, 0, 13, 10, 7, 0, 13, 7]
     keyArrNum = list(map(convertToNumbers, keyArr)) #[0, 1, 2, 3]
 
-    keyArrMatrix = [[keyArrNum[0], keyArrNum[1]], [keyArrNum[2], keyArrNum[3]]] #[[0, 1], [2, 3]]
+    # Do độ dài khóa K = 4 => m = K / 2 = 2 nên ta chia bản rõ và khóa K thành các đoạn con có độ dài bằng 2
+    keyArrMatrix = divideTheArray(keyArrNum) #[[0, 1], [2, 3]]
     plaintextArrNumAfter = divideTheArray(inputPlaintextArrNum) #[[13, 6], [20, 24], [4, 13], [21, 0], [13, 10], [7, 0], [13, 7]]
     
+    # Kết quả sau khi tính toán nhân cộng từ bản rõ với khóa K và chia lấy dư cho 26 ta được kết quả sau
     plaintextArrNumEncryption = numEncryption(plaintextArrNumAfter, keyArrMatrix) #[[6, 18], [24, 8], [13, 21], [0, 16], [10, 4], [0, 14], [7, 21]]
     
+    # Kết quả sau khi tính được ta sẽ làm phẳng mảng 2 chiều ở trên về mảng 1 chiều
     flatPlaintextNum = list(itertools.chain.from_iterable(plaintextArrNumEncryption)) #[6, 18, 24, 8, 13, 21, 0, 16, 10, 4, 0, 14, 7, 21]
+    
     
     alphabetJoin = ''.join(alphabets) #ABCDEFGHIJKLMNOPQRSTUVWXYZ
     
+    # Chuyển từ mảng số sang mảng ký tự dựa vào vị trí của chuỗi alphabet
     stringEncryptionArr = convertToString(alphabetJoin, flatPlaintextNum) #['G', 'S', 'Y', 'I', 'N', 'V', 'A', 'Q', 'K', 'E', 'A', 'O', 'H', 'V']
     
     resultEncryption = ''.join(stringEncryptionArr) #GSYINVAQKEAOHV - Kết quả đã được mã hóa từ bản rõ NGUYENVANKHANH với key là ABCD
@@ -73,6 +94,7 @@ def mainReset():
     tb_x1.delete(0, END)
     tb_x2.delete(0, END)
 
+# Khởi tạo form cho chương trình
 gpt = Tk()
 
 gpt.title("Hill encryption")
@@ -95,10 +117,10 @@ titleResult.pack()
 tb_x1 = Entry(gpt, width=30, font=("Consolas", 12))
 tb_x1.pack()
 
-btnEncryption = Button(gpt, text="Mã hóa",font=("Consolas", 15), fg="black", bg="orange", width=10, height=1, command=mainEncryption)
+btnEncryption = Button(gpt, text="Mã hóa",font=("Consolas", 15), fg="white", bg="blue", width=10, height=1, command=mainEncryption)
 btnEncryption.pack()
 
-btnDecryption = Button(gpt, text="Giải mã",font=("Consolas", 15), fg="black", bg="orange", width=10, height=1, command=mainDecryption)
+btnDecryption = Button(gpt, text="Giải mã",font=("Consolas", 15), fg="white", bg="blue", width=10, height=1, command=mainDecryption)
 btnDecryption.pack()
 
 titleDe = Label(gpt, text="Kết quả giải mã", font=("Arial" "bold"), fg="red")
@@ -107,7 +129,7 @@ titleDe.pack()
 tb_x2 = Entry(gpt, width=30, font=("Consolas", 12))
 tb_x2.pack()
 
-btnReset = Button(gpt, text="Reset",font=("Consolas", 15), fg="black", bg="orange", width=10, height=1, command=mainReset)
+btnReset = Button(gpt, text="Reset",font=("Consolas", 15), fg="white", bg="blue", width=10, height=1, command=mainReset)
 btnReset.pack()
 
 gpt.mainloop()
